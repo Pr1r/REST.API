293b99644c1fe57e28dd293f04ebf50e6865be2b
Pr1r <114554083+Pr1r@users.noreply.github.com>
2024-11-22 00:22:42 +0400

    Add files via upload

---
+++ controllers/HotelBookingControllerBase.cs
@@ -0,0 +1,6 @@
+﻿namespace HotelBookingAPI.Controllers
+{
+    public class HotelBookingControllerBase
+    {
+    }
+}
---
+++ controllers/IHotelBookingController.cs
@@ -0,0 +1,12 @@
+﻿using HotelBookingAPI.Controllers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookingAPI.Controllers
+{
+    public interface IHotelBookingController
+    {
+        JsonResult CreateEdit(HotelBooking booking);
+        JsonResult Delete(int id);
+        JsonResult Get(int id);
+    }
+}
